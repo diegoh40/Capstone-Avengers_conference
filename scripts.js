3d9e5e93254a7ecec955a4fec2e8d58093e6cff5
Diego <godie8a@hotmail.com>
2022-05-19 10:02:09 -0500

    Add files new

--- scripts.js
+++ scripts.js
@@ -35,33 +35,7 @@ const heros = [
         description: 'A massive dose of gamma radiation transformed the brilliant but meek scientist Bruce Banner DNA, awakening the hidden, adrenaline- fed hero in his genes...HULK!',
     },
 
-    {
-        image: './img/speakers/mob-markmanson.png',
-        name: 'Mark Manson',
-        title: 'Author of the New York Times bestselling book The Subtle Art of Not Giving a F*ck.',
-        description: 'As of 2019, he had authored three books, two of which were The New York Times bestsellers. His books have sold over 13 million copies.',
-    },
-
-    {
-        image: './img/speakers/mob-matthewwalker.png',
-        name: 'Matthew Walker',
-        title: 'Scientist and professor of neuroscience and psychology at the University of California, Berkeley',
-        description: 'He became a public intellectual following the publication of "Why We Sleep", his first work of popular science, in 2017. It became an international bestseller.',
-    },
-
-    {
-        image: './img/speakers/mob-niccomele.png',
-        name: 'Nicco Mele',
-        title: 'Academic, writer, and businessman. Author of "The End of Big"',
-        description: 'He is widely published on matters exploring the intersection of internet policy, new media, journalism, and politics.',
-    },
-
-    {
-        image: './img/speakers/mob-olarosling.png',
-        name: 'Ola Rosling',
-        title: 'Statistician known for his work for the Gapminder Foundation',
-        description: 'He co-authored the book "Factfulness" where suggests that the vast majority of people are wrong about the state of the world.',
-    },
+   
 ];
 
 const herosContainer = document.querySelector('.heros-container');
